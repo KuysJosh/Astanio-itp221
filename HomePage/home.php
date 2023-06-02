aa22bba4dbd43ef3120880cbd79f6c29da7efb09
KuysJosh <astaniojoshuan@gmail.com>
2023-06-01 21:18:47 -0700

    First Commit

---
+++ HomePage/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+    <div class="hero">
+        <nav>
+            <h6 class="logo">Information <br><span> Management</span> <br>System</h6>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Personal Information</a></li>
+                <li><a href="#">Educational Background</a></li>
+                <li><a href="#">Skills</a></li>
+                <li><a href="#">About us</a></li>
+            </ul>
+            <a href="../LoginPage/LoginPage1.html" class="btn">Logout</a>
+        </nav><br><br>
+
+        <div class="content">
+            <h1>Welcome <span>USER</span>,   <br> to the Information Management System.</h1>
+        </div>
+    </div>
+</body>
+</html>
---
+++ RegistrationPage/registration.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Login.css">
+</head>
+<body>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LogIn</title>
+    <link rel="stylesheet" href="registration.css">
+</head>
+<body>
+    <div class="center">
+    <h1>Registration Form</h1>
+    <div class="name" align="center">
+        <th><input type="text" required placeholder="First Name"></th>
+        <th><input type="text" required placeholder="Last Name"></th>
+    </div>
+    <form method="post">
+        <div class="txt_field">
+            <input type="text" required placeholder="Email">
+        </div>
+        <div class="txt_field">
+            <input type="password" required placeholder="Password">
+        </div>
+        <div class="txt_field">
+            <input type="password" required placeholder="Confirm Password">
+        </div>
+        <input type="checkbox" style="font-size: 15px;">I have read <a href="#"> <i>terms and policies</i></a></p>  
+         <center><input type="submit" value="Register Now"></center><br>
+         <p style="font-size: 15px;"><i>Already have an account?</i><a href="../LoginPage/LoginPage1.html"> Login instead</a></p><br><br>
+    </div>
+    </form>
+</div>
+</body>
+</html>
+</body>
+</html>
